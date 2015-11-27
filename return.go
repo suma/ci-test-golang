ccfbb4435f843a883e8a1eb58de3f015ab839dfd
Shuzo Kashihara <kashihara@preferred.jp>
2015-11-27 14:28:17 +0900

    Add cgo example

--- return.go
+++ return.go
@@ -1,5 +1,16 @@
 package citest
 
+/*
+int funcInt() {
+  return 100;
+}
+*/
+import "C"
+
+func returnInt() int {
+	return int(C.funcInt())
+}
+
 func returnString() string {
 	return "abc"
 }
--- return_test.go
+++ return_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+func TestReturnInt(t *testing.T) {
+	if returnInt() != 100 {
+		t.Errorf("it expected 100 but %v", returnInt())
+	}
+}
+
 func TestReturnString(t *testing.T) {
 	if returnString() != "ABC" {
 		t.Errorf("it expected 'ABC' but %v", returnString())
